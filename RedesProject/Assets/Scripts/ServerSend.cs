4da1bc1f76ae4573e49f9df60d9d2496aeb0335a
Daniel Laplana Gimeno <daniel.laplana.gimeno@gmail.com>
2024-05-23 10:54:54 +0200

    ServerSend singleton

--- RedesProject/Assets/Scripts/ServerSend.cs
+++ RedesProject/Assets/Scripts/ServerSend.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,6 +8,20 @@ public class ServerSend : MonoBehaviour
 {
     // Reference to the TextMeshPro Text Component
     public TMP_Text textMeshPro;
+
+    public static ServerSend instance = null; 
+    
+    public void Awake()
+    {
+        // Singleton
+        if (instance != null)
+        {
+            Debug.Log("Server send is a singleton");
+            return; 
+        }
+
+        instance = this;
+    }
 
     // Method to update the TextMeshPro text
     public void UpdateText(string newText)
